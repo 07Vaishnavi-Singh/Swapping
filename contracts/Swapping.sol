34617c88f3e2d2c66ddccae2180e80575cb3d5d1
07Vaishnavi-Singh <100015197+07Vaishnavi-Singh@users.noreply.github.com>
2023-06-12 19:16:31 +0530

    contract

---
+++ contracts/Swapping.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// just see if this gives and error while compiling
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/*
+How to swap tokens
+
+1. Alice has 100 tokens from AliceCoin, which is a ERC20 token.
+2. Bob has 100 tokens from BobCoin, which is also a ERC20 token.
+3. Alice and Bob wants to trade 10 AliceCoin for 20 BobCoin.
+4. Alice or Bob deploys TokenSwap                                       // see this and change this 
+5. Alice approves TokenSwap to withdraw 10 tokens from AliceCoin
+6. Bob approves TokenSwap to withdraw 20 tokens from BobCoin
+7. Alice or Bob calls TokenSwap.swap()                                   
+8. Alice and Bob traded tokens successfully.
+*/
+
+contract TokenSwap {
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint public amount2; // from the API
+
+    // take the amount of token 2 from the API
+    constructor(
+        address _token1,
+        address _owner1,
+        uint _amount1,
+        address _token2,
+        address _owner2,
+        uint _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
+
+    // function swap() public {
+    //     require(msg.sender == owner1 || msg.sender == owner2 , "Not authorized");
+    //     // require(msg.sender == owner1 || msg.sender == owner2 , "Not authorized");
+    //     require(
+    //         token1.allowance(owner1, address(this)) >= amount1,
+    //         "Token 1 allowance too low"
+    //     );
+    //     require(
+    //         token2.allowance(owner2, address(this)) >= amount2,  // fetch from he API
+    //         "Token 2 allowance too low"
+    //     );
+
+    //     _safeTransferFrom(token1, owner1, owner2, amount1);
+    //     _safeTransferFrom(token2, owner2, owner1, amount2);
+    // }
+
+    function fillQuote(
+        // The `fromTokenAddress` field from the API response.
+        string fromTokenAddress,
+        // The `toTokenAddress` field from the API response.
+        string toTokenAddress,
+        string protocols ,                                            // who should be mad ethe spender-the protocols?
+        // The `amounte` field from the API response.
+        string amount
+    ) public returns (uint256) {
+        require(spender != address(0), "Please provide a valid address");
+        // Track our balance of the buyToken to determine how much we've bought.
+        uint256 boughtAmount = toTokenAddress.balanceOf(address(this));
+        fromTokenAddress.approve(protocols, type(uint128).max);   
+               swapCallData =            protocols.part                    // how to set the swp data value
+        (bool success, ) = swapTarget.call{value: 0}(swapCallData);            // how to get the swap target 
+        emit ZeroXCallSuccess(success, boughtAmount);
+        require(success, "SWAP_CALL_FAILED");
+        boughtAmount = toTokenAddress.balanceOf(address(this)) - boughtAmount;
+        emit buyTokenBought(boughtAmount);
+        return boughtAmount;
+    }
+
+
+function swap(
+        IERC20[] calldata sellToken,
+        IERC20[] calldata buyToken,
+        address[] calldata spender,
+        address payable[] calldata swapTarget,
+        bytes[] calldata swapCallData,
+        uint256[] memory amount
+    ) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function _safeTransferFrom(
+        IERC20 token,
+        address sender,
+        address recipient,
+        uint amount
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "Token transfer failed");
+    }
+}
